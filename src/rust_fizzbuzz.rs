465c491108d9f7bc26b835b4d163c2297a98cfd2
proshunsuke <shunsuke0901@gmail.com>
2015-07-12 23:22:26 +0900

    実装書いた

--- src/rust_fizzbuzz.rs
+++ src/rust_fizzbuzz.rs
@@ -1,5 +1,6 @@
 pub fn fizzbuzz(n: i32) -> String{
     match n {
+        n if n % 5 == 0 => "buzz".to_string(),
         n if n % 3 == 0 => "fizz".to_string(),
         _               => n.to_string()
     }
