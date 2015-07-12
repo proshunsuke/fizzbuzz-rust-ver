ab242e1c1e0b8f629235ed658b03bb66c0cbd0ef
proshunsuke <shunsuke0901@gmail.com>
2015-07-12 23:46:47 +0900

    リファクタリング

--- src/rust_fizzbuzz.rs
+++ src/rust_fizzbuzz.rs
@@ -1,9 +1,9 @@
 pub fn fizzbuzz(n: i32) -> String {
-    match n {
-        n if n % 5 == 0  && n % 3 == 0 => "fizzbuzz".to_string(),
-        n if n % 5 == 0 => "buzz".to_string(),
-        n if n % 3 == 0 => "fizz".to_string(),
-        _               => n.to_string()
+    match (n % 3, n % 5) {
+        (0, 0) => "fizzbuzz".to_string(),
+        (_, 0) => "buzz".to_string(),
+        (0, _) => "fizz".to_string(),
+        _      => n.to_string()
     }
 }
 
